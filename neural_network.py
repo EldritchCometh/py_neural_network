c7816930e39f4e5e2720efdee4edb01a684bf75b
admin01 <abanzhof@gmail.com>
2023-04-30 17:32:15 -0400

    moved IO to the top

--- neural_network.py
+++ neural_network.py
@@ -7,6 +7,58 @@ from time import time
 from datetime import datetime, timedelta
 
 
+class IO:
+
+    @staticmethod
+    def get_samples():
+        with gzip.open('mnist_data.pkl.gz', 'rb') as f:
+            data = pickle.load(f)
+        return data['training_samples'], data['testing_samples']
+
+    @staticmethod
+    def get_biases(neurons):
+        return [[n.bias for n in l] for l in neurons]
+
+    @staticmethod
+    def get_weights(connections):
+        return [[[c.weight for c in g] for g in l] for l in connections]
+
+    @staticmethod
+    def set_biases(target, source):
+        for t_layer, s_layer in zip(target, source):
+            for t_neuron, s_bias in zip(t_layer, s_layer):
+                t_neuron.bias = s_bias
+
+    @staticmethod
+    def set_weights(target, source):
+        for t_layer, s_layer in zip(target, source):
+            for t_group, s_group in zip(t_layer, s_layer):
+                for t_conn, s_weight in zip(t_group, s_group):
+                    t_conn.weight = s_weight
+
+    @classmethod
+    def save_model(cls, nn, model_name):
+        models = {}
+        if os.path.isfile('models.pkl'):
+            with open('models.pkl', 'rb') as f:
+                models = pickle.load(f)
+        biases = cls.get_biases(nn.neurons)
+        weights = cls.get_weights(nn.connections)
+        models[model_name] = (biases, weights)
+        with open('models.pkl', 'wb') as f:
+            pickle.dump(models, f)
+
+    @classmethod
+    def load_model(cls, model_name):
+        with open('models.pkl', 'rb') as f:
+            models = pickle.load(f)
+        shape = [len(layer) for layer in models[model_name][0]]
+        neurons, connections = Architect.build(shape)
+        cls.set_biases(neurons, models[model_name][0])
+        cls.set_weights(connections, models[model_name][1])
+        return neurons, connections
+
+
 class Neuron:
 
     def __init__(self):
@@ -155,59 +207,6 @@ class Network:
         return [float(i == classification) for i in range(10)]
 
 
-class Trainer:
-
-    def __init__(self, nn, samples, ev=None):
-        self.nn = nn
-        self.samples = samples
-        self.ev = ev
-        self.training_mins = 1
-
-    def zero_out_batch_error_gradient_sums(self):
-        for layer in self.nn.neurons[1:-1]:
-            for neuron in layer:
-                neuron.batch_error_gradients_sum = 0
-
-    def set_error_gradients_in_output_layer(self, sample):
-        self.nn.output(sample['pixels'])
-        for neuron, target in zip(self.nn.neurons[-1], sample['one_hot']):
-            neuron.error_gradient = neuron.activation - target
-
-    def backpropagate(self):
-        for layer in reversed(self.nn.neurons[1:-1]):
-            for neuron in layer:
-                neuron.compute_and_update_error_gradient()
-
-    def backpropagation(self, batch):
-        for sample in batch:
-            self.zero_out_batch_error_gradient_sums()
-            self.set_error_gradients_in_output_layer(sample)
-            self.backpropagate()
-
-    def gradient_descent(self, learning_rate):
-        # Adjust biases by gradient descent
-        for neuron_layer in self.nn.neurons[1:]:
-            for neuron in neuron_layer:
-                neuron.descend_bias_gradient(learning_rate)
-        # Adjust weights by gradient descent
-        for conn_layer in self.nn.connections:
-            for conn_group in conn_layer:
-                for connection in conn_group:
-                    connection.descend_weight_gradient(learning_rate)
-
-    def train_network(self, learning_rate=0.01, batch_size=1):
-        learning_rate /= batch_size
-        start_time = time()
-        while (time() - start_time) / 60 < self.training_mins:
-            batch = random.sample(self.samples, batch_size)
-            self.backpropagation(batch)
-            self.gradient_descent(learning_rate)
-            if self.ev:
-                self.ev.basic_report(start_time)
-        if self.ev:
-            self.ev.final_report(learning_rate, batch_size, start_time)
-
-
 class Evaluator:
 
     def __init__(self, neural_network, samples):
@@ -265,56 +264,56 @@ class Evaluator:
         print('###############################################################')
 
 
-class IO:
+class Trainer:
 
-    @staticmethod
-    def get_samples():
-        with gzip.open('mnist_data.pkl.gz', 'rb') as f:
-            data = pickle.load(f)
-        return data['training_samples'], data['testing_samples']
+    def __init__(self, nn, samples, ev=None):
+        self.nn = nn
+        self.samples = samples
+        self.ev = ev
 
-    @staticmethod
-    def get_biases(neurons):
-        return [[n.bias for n in l] for l in neurons]
+    def zero_out_batch_error_gradient_sums(self):
+        for layer in self.nn.neurons[1:-1]:
+            for neuron in layer:
+                neuron.batch_error_gradients_sum = 0
 
-    @staticmethod
-    def get_weights(connections):
-        return [[[c.weight for c in g] for g in l] for l in connections]
+    def set_error_gradients_in_output_layer(self, sample):
+        self.nn.output(sample['pixels'])
+        for neuron, target in zip(self.nn.neurons[-1], sample['one_hot']):
+            neuron.error_gradient = neuron.activation - target
 
-    @staticmethod
-    def set_biases(target, source):
-        for t_layer, s_layer in zip(target, source):
-            for t_neuron, s_bias in zip(t_layer, s_layer):
-                t_neuron.bias = s_bias
+    def backpropagate(self):
+        for layer in reversed(self.nn.neurons[1:-1]):
+            for neuron in layer:
+                neuron.compute_and_update_error_gradient()
 
-    @staticmethod
-    def set_weights(target, source):
-        for t_layer, s_layer in zip(target, source):
-            for t_group, s_group in zip(t_layer, s_layer):
-                for t_conn, s_weight in zip(t_group, s_group):
-                    t_conn.weight = s_weight
+    def backpropagation(self, batch):
+        for sample in batch:
+            self.zero_out_batch_error_gradient_sums()
+            self.set_error_gradients_in_output_layer(sample)
+            self.backpropagate()
 
-    @classmethod
-    def save_model(cls, nn, model_name):
-        models = {}
-        if os.path.isfile('models.pkl'):
-            with open('models.pkl', 'rb') as f:
-                models = pickle.load(f)
-        biases = cls.get_biases(nn.neurons)
-        weights = cls.get_weights(nn.connections)
-        models[model_name] = (biases, weights)
-        with open('models.pkl', 'wb') as f:
-            pickle.dump(models, f)
+    def gradient_descent(self, learning_rate):
+        # Adjust biases by gradient descent
+        for neuron_layer in self.nn.neurons[1:]:
+            for neuron in neuron_layer:
+                neuron.descend_bias_gradient(learning_rate)
+        # Adjust weights by gradient descent
+        for conn_layer in self.nn.connections:
+            for conn_group in conn_layer:
+                for connection in conn_group:
+                    connection.descend_weight_gradient(learning_rate)
 
-    @classmethod
-    def load_model(cls, model_name):
-        with open('models.pkl', 'rb') as f:
-            models = pickle.load(f)
-        shape = [len(layer) for layer in models[model_name][0]]
-        neurons, connections = Architect.build(shape)
-        cls.set_biases(neurons, models[model_name][0])
-        cls.set_weights(connections, models[model_name][1])
-        return neurons, connections
+    def train_network(self, learning_rate=0.01, batch_size=1, mins=1.0):
+        learning_rate /= batch_size
+        start_time = time()
+        while (time() - start_time) / 60 < mins:
+            batch = random.sample(self.samples, batch_size)
+            self.backpropagation(batch)
+            self.gradient_descent(learning_rate)
+            if self.ev:
+                self.ev.basic_report(start_time)
+        if self.ev:
+            self.ev.final_report(learning_rate, batch_size, start_time)
 
 
 if __name__ == '__main__':
@@ -324,5 +323,5 @@ if __name__ == '__main__':
     network = Network(structure)
     evaluator = Evaluator(network, testing_samples)
     trainer = Trainer(network, training_samples, evaluator)
-    trainer.train_network()
-    IO.save_model(network, 'model01')
+    trainer.train_network(mins=60)
+    IO.save_model(network, 'batch_size_3')
